7f6a357ffe0159c5218347d551efe65a78f87193
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2023-11-11 14:21:05 +0530

    home.js

---
+++ home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to E – Gas Sewa (EGS)</h2>
+      <p>Providing in-depth services for gas agencies of Indian companies.</p>
+    </div>
+  );
+};
+
+export default Home;
