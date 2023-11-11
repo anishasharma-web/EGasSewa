408b214f3646acea4da874f1015c725604b7cea3
anishasharma-web <56109423+anishasharma-web@users.noreply.github.com>
2023-11-11 14:31:23 +0530

    ServiceDetails.js

---
+++ ServiceDetails.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ServiceDetails = () => {
+  return (
+    <div>
+      <h2>Our Services</h2>
+      <p>
+        Explore our services that make the path for LPG Gas Connection and other services a smooth one.
+      </p>
+    </div>
+  );
+};
+
+export default ServiceDetails;
